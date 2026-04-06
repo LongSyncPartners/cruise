a2ae77cb3a6002def376bca5d910b117b6001dba
LongSyncPartners <longnp@sync-partners.com>
2026-04-06 11:36:36 +0900

    ファイルを管理するように

---
+++ client/src/routes/AppRoutes.tsx
@@ -0,0 +1,40 @@
+﻿import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "@/pages/Login";
+import Dashboard from "@/pages/Dashboard";
+import Forecast from "@/pages/Forecast";
+import { AuthLayout, MainLayout } from "@/layouts";
+import { AuthProvider } from "../pages/Login/AuthContext";
+import Properties from "@/features/properties";
+import PropertyIncomeExpenseDetail from "@/features/property-income-expense-detail";
+
+
+export default function AppRoutes() {
+    return (
+        <AuthProvider>
+        <BrowserRouter>
+            <Routes>
+                {/* Auth */}
+                <Route element={<AuthLayout />}>
+                    <Route path="/login" element={
+                            <Login />
+                            } />
+                </Route>
+
+                {/* Main */}
+                <Route element={<MainLayout />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/forecast" element={<Forecast />} />
+                    <Route path="/properties" element={<Properties />} />
+                    <Route
+                        path="/properties/:propertyCode/finance"
+                        element={<PropertyIncomeExpenseDetail />}
+                    />
+                </Route>
+
+                 {/* fallback */}
+                <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+        </BrowserRouter>
+        </AuthProvider>
+    );
+}
